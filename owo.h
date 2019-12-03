58e9ef2cef6f98d0e538a4b0552cffb994b3ba27
Archaes Sima <illyasvielveinz11@gmail.com>
2019-12-03 17:43:09 +0700

    wip, not usable btw

---
+++ owo.h
@@ -0,0 +1,47 @@
+#ifndef _OWO_H_
+#define _OWO_H_
+
+#include <stdio.h>
+
+#define _Awignas     _Alignas
+#define _Awignof     _Alignof
+#define _Awoic       _Atomic
+#define _Owool       _Bool
+#define _Cowplex     _Complex
+#define _Genyeric    _Generic
+#define _Imaginyawy  _Imaginary
+#define _Nyowetuwn   _Noreturn
+#define owtwo        auto
+#define bweak        break
+#define casowo       case
+#define chaw         char
+#define conyst       const
+#define continyue    continue
+#define defyawut     default
+#define dowo         do
+#define dowoble      double
+#define ewse         else
+#define enyum        enum
+#define extewn       extern
+#define fyowoat      float
+#define fyow         for
+#define gowoto       goto
+#define iwfy         if
+#define inwinye      inline
+#define inytw        int
+#define wong         long
+#define wegistew     register
+#define wetuwn       return
+#define showt        short
+#define signyed      signed
+#define sizeowofy    sizeof
+#define stowotic     static
+#define sowoitch     switch
+#define typedeowofy  typedef
+#define uwunyion     union
+#define uwusignyed   unsigned
+#define vowoid       void
+#define vowowatiwe   volatile
+#define whiwe        while
+
+#endif
